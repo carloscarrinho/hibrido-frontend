4ba3ce58b92b11f007ada396d1a071c1a6d03c54
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2020-07-08 23:19:47 -0300

    It implements products swiper section

--- js/swiper-container.js
+++ js/swiper-container.js
@@ -1,37 +1,21 @@
-var appendNumber = 4;
-    var prependNumber = 1;
-    var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 3,
-      centeredSlides: true,
-      spaceBetween: 30,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-    document.querySelector('.prepend-2-slides').addEventListener('click', function (e) {
-      e.preventDefault();
-      swiper.prependSlide([
-        '<div class="swiper-slide">Slide ' + (--prependNumber) + '</div>',
-        '<div class="swiper-slide">Slide ' + (--prependNumber) + '</div>'
-      ]);
-    });
-    document.querySelector('.prepend-slide').addEventListener('click', function (e) {
-      e.preventDefault();
-      swiper.prependSlide('<div class="swiper-slide">Slide ' + (--prependNumber) + '</div>');
-    });
-    document.querySelector('.append-slide').addEventListener('click', function (e) {
-      e.preventDefault();
-      swiper.appendSlide('<div class="swiper-slide">Slide ' + (++appendNumber) + '</div>');
-    });
-    document.querySelector('.append-2-slides').addEventListener('click', function (e) {
-      e.preventDefault();
-      swiper.appendSlide([
-        '<div class="swiper-slide">Slide ' + (++appendNumber) + '</div>',
-        '<div class="swiper-slide">Slide ' + (++appendNumber) + '</div>'
-      ]);
-    });
+var swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 2,
+    stretch: 0,
+    depth: 50,
+    modifier: 0,
+    slideShadows: false,
+  },
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
